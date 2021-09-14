e2e2e2a6c32a751885b2e076394a950f92e545f5
mh17817 <mh17817@essex.ac.uk>
2021-09-14 08:08:02 +0100

    commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  googleMapsAPIKey: 'AIzaSyCic5ouWrcAVpjKnhyigUUUtPdm3mePuS4',
+  firebaseAPIKey: 'https://ionic-angular-football-default-rtdb.firebaseio.com/'
+};
